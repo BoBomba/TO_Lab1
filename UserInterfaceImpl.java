f601fabaf6c558540ef193b522d9ed5a87ee2e46
BoBomba <122816717+BoBomba@users.noreply.github.com>
2023-11-07 09:02:19 +0100

    fourth Commit the program is done all working

--- UserInterfaceImpl.java
+++ UserInterfaceImpl.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.math.*;
 
 public class UserInterfaceImpl implements UserInterface {
     private Singleton singleton;
@@ -52,21 +53,10 @@ public class UserInterfaceImpl implements UserInterface {
         Waluta zlotypolski = new Waluta("zloty (Polska)",1.0,"PLN",1.0);
         collection.addItem(zlotypolski);
 
-
-        Waluta W1 = exchange.getW1();
-        Waluta W2 = exchange.getW2();
-        double amount = exchange.getAmount();
-
-        if (W1 == null || W2 == null) {
-            System.out.println("Wybierz waluty do przeliczenia.");
-
-        }
-
-        Ccollection collection = Singleton.getInstance().getFormat().getCollection();
+        System.out.println("Wybierz waluty do przeliczenia.");
 
         // Znajdź obiekty W1 i W2 na podstawie kodu waluty
-        Waluta foundW1 = null;
-        Waluta foundW2 = null;
+
 
 
         Scanner scanner = new Scanner(System.in);
@@ -78,6 +68,9 @@ public class UserInterfaceImpl implements UserInterface {
 
         System.out.print("Podaj ilosc do przeliczenia: ");
         double ilosc = scanner.nextDouble();
+
+        Waluta W1 = null;
+        Waluta W2 = null;
 
 // Teraz możesz przypisać wczytane kody do odpowiednich obiektów W1 i W2
         exchange.setAmount(ilosc);
@@ -101,13 +94,17 @@ public class UserInterfaceImpl implements UserInterface {
             return 1;
         }
 
+        exchange.setW1(W1);
+        exchange.setW2(W2);
 
-//        double kursW1 = foundW1.getKurs();
-//        double kursW2 = foundW2.getKurs();
+        double kursW1 = W1.getKurs();
+        double kursW2 = W2.getKurs();
+
+        System.out.println("Kurs " + kodW1 + " " + kursW1 + " Kurs " + kodW2 + " " + kursW2);
 
         double result = exchange.result();
 
-        System.out.println("Wynik przeliczenia: " + result);
+        System.out.println("Wynik przeliczenia z: "+ exchange.getAmount() + W1.getKodWaluty() + " to " + result + W2.getKodWaluty());
         return 0;
     }
 }
